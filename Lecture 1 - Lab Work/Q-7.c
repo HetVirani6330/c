221a8546b4bdb81a65e4d261a65d7d80b01d4ea0
HetVirani6330 <152472532+HetVirani6330@users.noreply.github.com>
2023-12-22 13:18:22 +0530

    Add files via upload

---
+++ Lecture 1 - Lab Work/Q-7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    
+    char i,j;
+
+    for(i='a';i<='e';i++){ 
+        for(j=i;j>='a';j--){
+            printf(" %c",j);
+        }
+        printf("\n");
+
+    }
+
+
+    return 0;
+}
+
+
+
